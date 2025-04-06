f07dfac2527078793f76e43c077926405333f519
Noé Fleury <23384755+noefleury@users.noreply.github.com>
2025-04-06 17:03:58 +0200

    chore: update test

--- tests/Unit/BasicTest.php
+++ tests/Unit/BasicTest.php
@@ -2,6 +2,7 @@
 
 namespace NoeFleury\InfomaniakSdk\Tests\Unit;
 
+use NoeFleury\InfomaniakSdk\Client;
 use NoeFleury\InfomaniakSdk\Enum\RequestBuilder\OrderDirection;
 use NoeFleury\InfomaniakSdk\Tests\TestCase;
 use PHPUnit\Framework\Attributes\CoversNothing;
@@ -19,6 +20,10 @@ class BasicTest extends TestCase
 
     public function test_thing() {
         $this->assertSame('asc', OrderDirection::Ascendant->value);
+    }
+
+    public function test_another() {
+        $this->assertInstanceOf(Client::class, new Client());
     }
 
 }
